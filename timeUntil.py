8f763a3c85731c295095a11d28678c774bab5d09
E-Justin <67754579+E-Justin@users.noreply.github.com>
2022-03-04 14:17:18 -0600

    Update timeUntil.py
    still want to add:
    * have user enter in what it is that they are counting down to
    * display the event they are counting down to in the final display of time

--- timeUntil.py
+++ timeUntil.py
@@ -18,35 +18,57 @@ class Time:
         self.futureHour = 0
         self.futureMinute = 0
         self.futureSecond = 0
-        self.hoursUnitl = 0
+        self.hoursUntil = 0
         self.minutesUntil = 0
         self.secondsUntil = 0
 
+#instantiate a class object
 CountDown = Time()
 
+# get the current time
 now = datetime.now()
 CountDown.currentTime = now.strftime("%m/%d/%Y, %H:%M:%S")
 # gets date/time in this format: 03/04/2022, 08:47:57
 #            index:              01234567890123456789
 
 
+#get time and assign it to appropriate variable
 CountDown.nowHour = int(CountDown.currentTime[12:14]) # extracts hour from time/date
 CountDown.nowMinute = int(CountDown.currentTime[15:17]) # extracts minute(s) from time/date
 CountDown.nowSecond = int(CountDown.currentTime[18:]) # extracts second(s) from time/ date
 
 
+# get user to enter in the time they want to count down to
+CountDown.futureTime = pyip.inputStr("Enter the time that you want to countdown to in standard time (hh:mm)")
+#! CountDown.futureTime = "04:15"   #for testing purposes
+CountDown.futureAMPM = pyip.inputChoice(["AM", "PM"], "is that AM or PM?")
+#! CountDown.futureAMPM = "PM"      #for testing purposes
 
-# CountDown.futureTime = pyip.inputStr("Enter the time that you want to countdown to in standard time (hh:mm)")
-CountDown.futureTime = "04:15"
-# CountDown.futureAMPM = pyip.inputChoice(["AM", "PM"], "is that AM or PM?")
-CountDown.futureAMPM = "PM"
+# assign time to appropriate variable
 CountDown.futureHour = int(CountDown.futureTime[0:2])
 if CountDown.futureAMPM == "PM":
-    CountDown.futureHour += 12
+    CountDown.futureHour += 12 # convert to military time to make calculations easier
 CountDown.futureMinute = int(CountDown.futureTime[3:5])
 
-print(str(CountDown.futureHour) + ":" + str(CountDown.futureMinute))
-print(str(CountDown.nowHour) + ":" + str(CountDown.nowMinute))
+
+# Calculate how long until the entered time
+CountDown.secondsUntil = (60 - CountDown.nowSecond)
+
+if CountDown.futureMinute > CountDown.nowMinute:
+    CountDown.minutesUntil = CountDown.futureMinute - CountDown.nowMinute
+    
+else:
+    CountDown.minutesUntil = ((60 - CountDown.nowMinute) + CountDown.futureMinute)
+    CountDown.hoursUntil -= 1
+
+CountDown.hoursUntil += (CountDown.futureHour - CountDown.nowHour)
+
+
+# display how long until
+print("~~~~~~~ Time Until ~~~~~~~")
+print("Hours    " + str(CountDown.hoursUntil))
+print("Minutes  " + str(CountDown.minutesUntil))
+print("Seconds  " + str(CountDown.secondsUntil))
 
 
 
