1a55723a18efb2f8b0d7bb22726a2c0f273ded59
E-Justin <67754579+E-Justin@users.noreply.github.com>
2022-06-10 06:57:18 -0700

    Update stock_prices.py

--- stock_prices.py
+++ stock_prices.py
@@ -4,6 +4,9 @@ from datetime import datetime
 # import pandas as pd
 
 today = datetime.now()
+today = today.ctime() # gets date in this format: Fri Jun 10 08:50:57 2022
+
+stock_name = str
 
 headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/102.0.0.0 Safari/537.36'}
 
@@ -14,25 +17,30 @@ soup = BeautifulSoup(source.text, 'lxml') # creating soup object
 #page_text = soup.get_text()
 
 current_price = soup.find('span', {'class': 'IsqQVc NprOob wT3VGc'}) # the price is found in a <span with this class name
-print(current_price.get_text())
+# print(current_price.get_text())
 
 
 
 
-f = open("stock_prices.txt", "a+") # open file for appending/ create if it does not already exist
-f.write("\n %s : %s " % (today, current_price.get_text())) # write data to the file: (today's date/time) : (price at the moment the program is run)
-f.close() # close file when finished
 
+
+
+
+
+
+
+
+f = open("stock_prices.txt", "a+")
+f.write("\n %s : $%s " % (today, current_price.get_text()))
+f.close()
 
 '''to do:
 * add more stocks
 * open and closing price
 * how much it went up/ down from the previous day
-* format the date to make it more user friendly
+* 
 * put all of this in a table/ excel sheet
 * probably more '''
-
-
 
 
 
