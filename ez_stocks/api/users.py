97dbb45de9181a071ebad599b60df4bd3bf86d91
E-Justin <67754579+E-Justin@users.noreply.github.com>
2022-08-30 12:20:16 -0700

    Update users.py
    added a DELETE end point
    pw must be at least 8 chars
    name must be at least 3 chars

--- ez_stocks/api/users.py
+++ ez_stocks/api/users.py
@@ -1,6 +1,14 @@
 from flask import Blueprint, jsonify, abort, request
 from ..models import User, db
 
+import hashlib
+import secrets
+
+def scramble(password: str):
+    """ hash and salt the given password """
+    salt = secrets.token_hex(16)
+    return hashlib.sha512((password + salt).encode('utf-8')).hexdigest()
+
 bp = Blueprint('users', __name__, url_prefix= '/users')
 
 @bp.route('', methods=['GET'])
@@ -11,7 +19,7 @@ def index():
         result.append(user.serialize()) # build list of users as dictionary   
     return jsonify(result) # return JSON response
 
-@bp.route('', methods=['GET'])
+@bp.route('<int:id>', methods=['GET'])
 def show(id: int):
     user = User.query.get_or_404(id)
     return jsonify(user.serialize())
@@ -22,12 +30,26 @@ def create():
     # request body must contain: name, password, and email
     if 'name' not in request.json or 'password' not in request.json or 'email' not in request.json:
         return abort(400) # abort if requirments are not fulfilled
+    # name must be > 2 chars | password must be > 7 chars
+    if len(request.json['name']) < 3 or len(request.json['password']) < 8:
+        return abort(400)
     # construct new user
     user = User(
         name = request.json['name'],
-        password = request.json['password'],
+        password = scramble(request.json['password']),
         email = request.json['email']
     )
     db.session.add(user)  # prepare CREATE statement
     db.session.commit()  # execute CREATE statement
     return jsonify(user.serialize())
+
+@bp.route('/<int:id>', methods=['DELETE'])
+def delete(id: int):
+    user = User.query.get_or_404(id)  # query for user
+    try:
+        db.session.delete(user) # prepare DELETE statement
+        db.session.commit()  # execute DELETE statement
+        return jsonify(True)
+    except:
+        # something went wrong
+        return jsonify(False)
