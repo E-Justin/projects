a48f53efc4053d05ec9986b158b89eeab51b7f1a
E-Justin <67754579+E-Justin@users.noreply.github.com>
2022-08-31 06:54:56 -0700

    Update stocks.py

--- ez_stocks/api/stocks.py
+++ ez_stocks/api/stocks.py
@@ -5,6 +5,7 @@ bp = Blueprint('stocks', __name__, url_prefix='/stocks')
 
 @bp.route('', methods=['GET'])
 def index():
+    """ get all stocks """
     stocks = Stock.query.all()
     result = []
     for stock in stocks:
@@ -13,11 +14,13 @@ def index():
 
 @bp.route('/<int:id>', methods=['GET'])
 def show(id: int):
+    """ get 1 stock by id """
     stock = Stock.query.get_or_404(id)
     return jsonify(stock.serialize())
 
 @bp.route('', methods=['POST'])
 def create():
+    """ add new stock to db """
     # request body must contain:symbol, percent_change, 
     # price_change, current_price, name
     if 'symbol' not in request.json or 'percent_change' not in request.json or 'price_change' not in request.json or 'current_price' not in request.json or 'name' not in request.json:
@@ -35,6 +38,7 @@ def create():
 
 @bp.route('/<int:id>', methods=['DELETE'])
 def delete(id: int):
+    """ delete stock from db by id"""
     stock = Stock.query.get_or_404(id)  # query for stock
     try:
         # if all goes well
@@ -43,4 +47,18 @@ def delete(id: int):
         return jsonify(True)
     except:
         # if something goes wrong
+        return jsonify(False)
+
+@bp.route('/<int:id>', methods=['PATCH', 'PUT'])
+def update(id: int):
+    """ update stock info """
+    # check to see if stock exists
+    stock = Stock.query.get_or_404(id)
+    # must have name, symbol, current price, price change, and percent change in request
+    if 'symbol' not in request.json or 'name' not in request.json or 'current_price' not in request.json or 'percent_change' not in request.json or 'price_change' not in request.json:
+        return abort(400)
+    try:
+        db.session.commit()
+        return jsonify(stock.serialize())
+    except:
         return jsonify(False)
