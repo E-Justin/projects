f3e5de8beaeecf2e35f3f1737a1288b6e1232732
E-Justin <67754579+E-Justin@users.noreply.github.com>
2022-08-31 06:54:05 -0700

    Update users.py

--- ez_stocks/api/users.py
+++ ez_stocks/api/users.py
@@ -13,6 +13,7 @@ bp = Blueprint('users', __name__, url_prefix= '/users')
 
 @bp.route('', methods=['GET'])
 def index():
+    """ get all users """
     users = User.query.all() # ORM performs SELECT * FROM users table
     result = []
     for user in users:
@@ -21,12 +22,14 @@ def index():
 
 @bp.route('<int:id>', methods=['GET'])
 def show(id: int):
+    """ get 1 user from id """
     user = User.query.get_or_404(id)
     return jsonify(user.serialize())
 
 
 @bp.route('', methods=['POST'])
 def create():
+    """ create new user"""
     # request body must contain: name, password, and email
     if 'name' not in request.json or 'password' not in request.json or 'email' not in request.json:
         return abort(400) # abort if requirments are not fulfilled
@@ -36,7 +39,7 @@ def create():
     # construct new user
     user = User(
         name = request.json['name'],
-        password = scramble(request.json['password']),
+        password = scramble(request.json['password']), # encrypt password before storing in db
         email = request.json['email']
     )
     db.session.add(user)  # prepare CREATE statement
@@ -45,6 +48,7 @@ def create():
 
 @bp.route('/<int:id>', methods=['DELETE'])
 def delete(id: int):
+    """ delete user from id """
     user = User.query.get_or_404(id)  # query for user
     try:
         db.session.delete(user) # prepare DELETE statement
@@ -53,3 +57,49 @@ def delete(id: int):
     except:
         # something went wrong
         return jsonify(False)
+
+@bp.route('/<int:id>', methods = ['PATCH', 'PUT'])
+def update(id:int):
+    """ update user info 
+        can update username, pw, or both """
+    # check to see if user exists
+    user = User.query.get_or_404(id)
+    # name and pw must be in request
+    if 'name' not in request.json and 'password' not in request.json and 'email' not in request.json:
+        return abort(400)
+    
+    if 'name' in request.json:
+        # name must be at least 3 characters long
+        if len(request.json['name']) < 3:
+            # if name is too short (> 3)
+            return abort(400)
+        else: # if name is appropriate length
+            # update name
+            user.name = request.json['name']
+    
+    if 'password' in request.json:
+    # pw must be at least 8 characters long
+        if len(request.json['password']) < 8:
+            # if pw is not long enough
+            return abort(400)
+        else:
+            # encrypt password and store to db
+            user.password = scramble(request.json['password'])
+
+    if 'email' in request.json:
+        if not '@' in request.json['email']:
+            # email must have @ to be valid
+            return abort(400)
+        else:
+            # update pw
+            user.email = request.json['email']
+
+    
+    try: # if everything goes well
+        # commit change
+        db.session.commit()
+        return jsonify(user.serialize())
+    except:
+        # if something went wrong
+        return jsonify(False)
+
